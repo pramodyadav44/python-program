64c01e83039a25607ad88fe9d7fe80dc58e696d5
Pramod-sj <pramodsinghjantwal@gmail.com>
2016-10-14 10:41:53 +0530

    Add files via upload

---
+++ prac2semicoloninput (1).py
@@ -0,0 +1,15 @@
+#PRACTICAL - 2
+#EXECUTING SEMICOLON SEPARATED SEQUENCE OF STATEMNTS
+2+3;4**4;100//3;print('hello');x=None;print("The value of x is",x)
+
+str=input("Enter your name....\n")
+print("The name is ",str)
+
+
+str1=list(input("enter string...\n"))
+print("The list of characters is",str1)
+
+print(len(str1))
+print(min(str))
+print(max(str))
+
